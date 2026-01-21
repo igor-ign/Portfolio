a5c4738f3e8375ee77267ea90deae05050080958
Igor Ignácio <euleiodps@gmail.com>
2026-01-21 20:38:22 -0300

    feat(observability): added basic observability features to the project (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import { StageProvider } from './context';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import { StageProvider } from './context'
+import App from './App'
 import './i18n'
-import './index.scss';
+import './index.scss'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+setupLogRocketReact()
+
+LogRocket.init('f8fpck/portfolio')
+LogRocket.log('Portfolio app initialized')
+LogRocket.identify(window.navigator.language, {
+  userAgent: window.navigator.userAgent
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StageProvider>
     <App />
   </StageProvider>
-);
+)
